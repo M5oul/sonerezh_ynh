50c130fadb9a1d43c6888a3983c849b399025844
ericgaspar <junk.eg@free.fr>
2020-09-10 23:42:16 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-mysql and php${YNH_PHP_VERSION}-gd ffmpeg"
+pkg_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd ffmpeg"
 
 YNH_PHP_VERSION="7.3"
 
